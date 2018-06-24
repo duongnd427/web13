12c21646afaaead949fe886167651fa60bce6bc5
tititixxx <duong427@gmail.com>
2018-06-25 00:58:55 +0700

    ex day2

--- js-practices/Practice2/sortPractice.js
+++ js-practices/Practice2/sortPractice.js
@@ -5,11 +5,11 @@ function sort(input) {
   while (true) {
     isSorted = true;
     for (var i = 0; i < input.length - 1; i++) {
-      if (input[i] > input[i+1]) {
+      if (input[i] > input[i + 1]) {
         isSorted = false;
         var temp = input[i];
-        input[i] = input[i+1];
-        input[i+1] = temp;
+        input[i] = input[i + 1];
+        input[i + 1] = temp;
       }
     }
     if (isSorted === true) return input;
