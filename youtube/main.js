3260768c5e7ac562d402fa2723768e131776ef57
duongnd427 <duong427@gmail.com>
2018-07-30 22:36:16 +0700

    loading

--- youtube/main.js
+++ youtube/main.js
@@ -1,20 +1,41 @@
+var pageToken;
+var isLoading = false;
+var keyword;
+var debounceTimeout;
 $(document).ready(function () {
-    $("#search").on("submit", function (event) {
-        event.preventDefault();
-        window.onload=function(){
-            window.setTimeout('document.search.submit()', 500)
-        } 
 
-        $.ajax({
-            url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q='+$('#keyword').val()+'&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw',
-            type: 'GET',            
-            success: function (result) {
-                console.log(result)   
+    $("#search").on("input", function (event) {
+        event.preventDefault();
+        $("#result-list").empty();
+        $(".loader").css("opacity", "1");
+
+        keyword = $('#keyword').val();
+        pageToken = '';
+
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(function () {
+            if (keyword)
+                search(keyword)
+            else {
                 $("#result-list").empty();
-                for(var i=0; i<result.items.length;i++)
-                {
+                $(".loader").css("opacity", "0")
+            }
+        }
+            , 1000);
+    })
+});
+
+
+function search(keyword) {
+    $.ajax({
+        url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${keyword}&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw&pageToken=${pageToken}`,
+        type: 'GET',
+        success: function (result) {
+            console.log(result)            
+            pageToken = result.nextPageToken ? result.nextPageToken : null;
+            for (var i = 0; i < result.items.length; i++) {
+                if (result.items[i].id.kind == "youtube#video") {
                     $("#result-list").append(`
-                    <div class="col-md-12">
                         <a class="result col-md-12" href="https://www.youtube.com/watch?v=${result.items[i].id.videoId}?autoplay=true" target="_blank">
                             <img src="${result.items[i].snippet.thumbnails.medium.url}" alt"">
                             <div class="${result.items[i].snippet}">
@@ -23,42 +44,25 @@ $(document).ready(function () {
                                 <span>View>></span>
                             </div>
                         </a>
-                    </div>
-                    `)       
+                    `)
                 }
-                $(window).scroll(function() {
-                    if($(window).scrollTop() + $(window).height() == $(document).height()) {
-                        var j=i;
-                        i+=25;
-                        var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+i+'&q='+$('#keyword').val()+'&key=AIzaSyA9gQZ-oYomFypZN7PsupZJtOfQqA6Q3qw';;
-                        $.ajax({
-                            url: url,
-                            type: 'GET',            
-                            success: function (result) {
-                                console.log(result)                                
-                                for(j; j<result.items.length;j++)
-                                {
-                                    $("#result-list").append(`
-                                    <div class="col-md-12">
-                                        <a class="result col-md-12" href="https://www.youtube.com/watch?v=${result.items[j].id.videoId}?autoplay=true" target="_blank">
-                                            <img src="${result.items[j].snippet.thumbnails.medium.url}" alt"">
-                                            <div class="${result.items[j].snippet}">
-                                                <h2 class="title"> ${result.items[j].snippet.title}</h2>
-                                                <p class="description">${result.items[j].snippet.description}</p>
-                                                <span>View>></span>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    `)       
-                                }                                        
-                            },
-                             error: function() {
-                                alert("Hết video liên quan !!");
-                            }
-                        });
-                    }
-                });                         
             }
-        });
+            isLoading = false;
+        }
     });
-});
+}
+
+$(window).on("scroll", function () {
+    if ($(document).height() - $(window).height() - $(window).scrollTop() < 300) {
+        if (pageToken && !isLoading) {
+            isLoading = true;
+            //keyword = $("#search").val();
+            search(keyword);
+        }
+    }
+})
+
+var promiseFuntion = function () {
+
+}
+
